c063d96a1278175bbf6a11bd1de019d7610cbcf8
Malte Fiala <maltefiala@produktion.io>
2017-01-25 18:17:04 +0100

    Comment theme to use readthedocs

--- conf.py
+++ conf.py
@@ -85,7 +85,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -174,7 +174,7 @@ texinfo_documents = [
 # regardless of the global pdf_compressed setting.
 
 pdf_documents = [
-    ('index', u'S2A9900', u'S2A9900', u'Enproduktion GmbH'),
+    ('index', u'S2A9900-docs', u'S2A9900-docs', u'Enproduktion GmbH'),
 ]
 
 # # A comma-separated list of custom stylesheets. Example:
